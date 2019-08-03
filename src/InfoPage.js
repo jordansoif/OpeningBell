fd8bf61f48fd1829761dced90613a2b8344469af
Jordan <48663692+jordansoif@users.noreply.github.com>
2019-08-02 20:37:16 -0400

    prettier reformat

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,126 +1,146 @@
 import React from "react";
-import { Input, Button, DatePicker } from 'antd';
-import 'antd/dist/antd.css';
-import Axios from "axios"
+import { Input, Button, DatePicker } from "antd";
+import "antd/dist/antd.css";
+import Axios from "axios";
 import { Link } from "@reach/router";
-import moment from "moment"
+import moment from "moment";
 
 const divStyleRight = {
-    float: "right"
-}
+  float: "right"
+};
 
 class InfoPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            infoInput: "",
-            stockInfo: "",
-            day: "",
-            month: "",
-            year: "",
-            targetDateOpen: "",
-            targetDate: ""
-        }
-        this.dateValue = this.dateValue.bind(this);
-        this.historicalPrice = this.historicalPrice.bind(this)
-    }
-
-    componentWillMount () {
-        console.log(this.props);
-        this.setState({
-            infoInput: this.props.symbol,
-      }), Axios.get(`https://api.worldtradingdata.com/api/v1/stock?symbol=${this.props.symbol}&api_token=webEtV2VViO9zqzRoxhxVCoUtoG8JYkzJyXMwbmxBYG9RcYjMxh9KoKhejAB`)
-      .then(function (response) {
-          this.setState({
-              stockInfo: response.data
-              }
-          )
-      }.bind(this));
-      }
-
-    componentDidUpdate () {
-        Axios.put(`http://localhost:5000/stock/price/${this.props.symbol}`, {
-            price: this.state.stockInfo.data[0].price_open
-        }).then(res => {
-            console.log(res)
-        })
-    }
-
-    historicalPrice () {
-        var monthReformat
-        var dayReformat
-        
-        if (parseInt(this.state.day, 10) < 10) {
-            dayReformat = "0" + this.state.day
-        } else dayReformat = this.state.day;
-
-        if (parseInt(this.state.month, 10) < 10) {
-            monthReformat = "0" + this.state.month
-        } else monthReformat = this.state.month;
-
-        var formatDate = `${this.state.year}-${monthReformat}-${dayReformat}`;
-        
-        Axios.get(`https://api.worldtradingdata.com/api/v1/history?symbol=${this.state.infoInput}&sort=newest&api_token=webEtV2VViO9zqzRoxhxVCoUtoG8JYkzJyXMwbmxBYG9RcYjMxh9KoKhejAB`)
-        .then(function (response) {
-            console.log(response.data.history[formatDate]);
-            if (!response.data.history[formatDate]) { //undefined is falsy, so this checks if the value is undefined
-                return (this.setState({
-                    targetDateOpen: "Does Not Exist",
-                    targetDate: formatDate
-                 })
-            )}  else {
-                this.setState({
-                    targetDateOpen: response.data.history[formatDate].open,
-                    targetDate: formatDate
-                })
-            }
-    }.bind(this) )
+  constructor(props) {
+    super(props);
+    this.state = {
+      infoInput: "",
+      stockInfo: "",
+      day: "",
+      month: "",
+      year: "",
+      targetDateOpen: "",
+      targetDate: ""
     };
+    this.dateValue = this.dateValue.bind(this);
+    this.historicalPrice = this.historicalPrice.bind(this);
+  }
 
-    dateValue (e) {
-        this.setState({
-            day: moment(e).date(),
-            month: moment(e).month() + 1,
-            year: moment(e).year()
-        }); 
-    }
+  componentWillMount() {
+    console.log(this.props);
+    this.setState({
+      infoInput: this.props.symbol
+    }),
+      Axios.get(
+        `https://api.worldtradingdata.com/api/v1/stock?symbol=${
+          this.props.symbol
+        }&api_token=webEtV2VViO9zqzRoxhxVCoUtoG8JYkzJyXMwbmxBYG9RcYjMxh9KoKhejAB`
+      ).then(
+        function(response) {
+          this.setState({
+            stockInfo: response.data
+          });
+        }.bind(this)
+      );
+  }
 
-    render() {
-        return (
-            <div>
-            
-            <h1>{this.state.infoInput}</h1>
-            
-            <h2>{(this.state.stockInfo == "") ? "" :
-                (this.state.infoInput + " opened at a price of $") +  
-                this.state.stockInfo.data[0].price_open}
-            </h2>
-            
-            <h3>
-               {(this.state.targetDateOpen ==  (false)) ? "" : 
-               (this.state.targetDateOpen == "Does Not Exist") ? 
-               this.state.targetDateOpen :
-                ("The opening price of " + this.state.infoInput +
-                 " on " + this.state.targetDate + " was " +
-                this.state.targetDateOpen)}
-            </h3>
-            
-            <DatePicker size="large" onChange={this.dateValue}>Historical Lookup</DatePicker>
-            
-            <Button onClick={this.historicalPrice} size="large">
-                Historical Open Lookup
+  componentDidUpdate() {
+    Axios.put(`http://localhost:5000/stock/price/${this.props.symbol}`, {
+      price: this.state.stockInfo.data[0].price_open
+    }).then(res => {
+      console.log(res);
+    });
+  }
+
+  historicalPrice() {
+    var monthReformat;
+    var dayReformat;
+
+    if (parseInt(this.state.day, 10) < 10) {
+      dayReformat = "0" + this.state.day;
+    } else dayReformat = this.state.day;
+
+    if (parseInt(this.state.month, 10) < 10) {
+      monthReformat = "0" + this.state.month;
+    } else monthReformat = this.state.month;
+
+    var formatDate = `${this.state.year}-${monthReformat}-${dayReformat}`;
+
+    Axios.get(
+      `https://api.worldtradingdata.com/api/v1/history?symbol=${
+        this.state.infoInput
+      }&sort=newest&api_token=webEtV2VViO9zqzRoxhxVCoUtoG8JYkzJyXMwbmxBYG9RcYjMxh9KoKhejAB`
+    ).then(
+      function(response) {
+        console.log(response.data.history[formatDate]);
+        if (!response.data.history[formatDate]) {
+          //undefined is falsy, so this checks if the value is undefined
+          return this.setState({
+            targetDateOpen: "Does Not Exist",
+            targetDate: formatDate
+          });
+        } else {
+          this.setState({
+            targetDateOpen: response.data.history[formatDate].open,
+            targetDate: formatDate
+          });
+        }
+      }.bind(this)
+    );
+  }
+
+  dateValue(e) {
+    this.setState({
+      day: moment(e).date(),
+      month: moment(e).month() + 1,
+      year: moment(e).year()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.infoInput}</h1>
+
+        <h2>
+          {this.state.stockInfo == ""
+            ? ""
+            : this.state.infoInput +
+              " opened at a price of $" +
+              this.state.stockInfo.data[0].price_open}
+        </h2>
+
+        <h3>
+          {this.state.targetDateOpen == false
+            ? ""
+            : this.state.targetDateOpen == "Does Not Exist"
+            ? this.state.targetDateOpen
+            : "The opening price of " +
+              this.state.infoInput +
+              " on " +
+              this.state.targetDate +
+              " was " +
+              this.state.targetDateOpen}
+        </h3>
+
+        <DatePicker size="large" onChange={this.dateValue}>
+          Historical Lookup
+        </DatePicker>
+
+        <Button onClick={this.historicalPrice} size="large">
+          Historical Open Lookup
+        </Button>
+
+        <h4>
+          <Link to="/">
+            <Button type="primary" style={divStyleRight}>
+              Go to Homepage
             </Button>
-            
-            <h4>
-            <Link to="/">
-                <Button type="primary" style={divStyleRight} >Go to Homepage</Button>
-            </Link>
-            </h4>
-            
-            </div>
-        )
-    }
-};
+          </Link>
+        </h4>
+      </div>
+    );
+  }
+}
 
 export default InfoPage;
 
--- src/NotesPage.js
+++ src/NotesPage.js
@@ -1,67 +1,72 @@
-import React from 'react';
+import React from "react";
 import Axios from "axios";
-import { Link } from '@reach/router';
-import { Input, Button } from 'antd';
+import { Link } from "@reach/router";
+import { Input, Button } from "antd";
 
 const { TextArea } = Input;
 
 const divStyleRight = {
-    float: "right"
-}
+  float: "right"
+};
 
 class NotesPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = ""
-        this.currentNotes= "";
-        
-        this.updateNotes = this.updateNotes.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = "";
+    this.currentNotes = "";
 
-    updateNotes () {
-        var notes = document.getElementById("notesArea").value;  
-        console.log(notes); 
-        Axios.put(`http://localhost:5000/stock/notes/${this.props.symbol}`,{
-            notes: notes
-        }).then(res => {
-            this.setState({
-                currentNotes: res.data.notes
-            })
-        }).catch(err => {
-            console.log(err)
-        }) 
-    }
+    this.updateNotes = this.updateNotes.bind(this);
+  }
 
-    componentWillMount () {
-            Axios.get(`http://localhost:5000/stock/${this.props.symbol}`)
-            .then(res => {
-                this.setState({
-                    currentNotes: res.data.notes
-                })
-            }).catch(err => {
-                console.log(err)
-            })    
-        }
+  updateNotes() {
+    var notes = document.getElementById("notesArea").value;
+    console.log(notes);
+    Axios.put(`http://localhost:5000/stock/notes/${this.props.symbol}`, {
+      notes: notes
+    })
+      .then(res => {
+        this.setState({
+          currentNotes: res.data.notes
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Notes Page for {this.props.symbol}</h1>
-                <h2>Notes: {this.state.currentNotes}</h2>
-                <TextArea id="notesArea" rows={4} defaultValue= 'Change Notes'/>
+  componentWillMount() {
+    Axios.get(`http://localhost:5000/stock/${this.props.symbol}`)
+      .then(res => {
+        this.setState({
+          currentNotes: res.data.notes
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-                <div>
-                    <Button type="primary" onClick={this.updateNotes}>
-                        Submit Notes
-                    </Button>
+  render() {
+    return (
+      <div>
+        <h1>Notes Page for {this.props.symbol}</h1>
+        <h2>Notes: {this.state.currentNotes}</h2>
+        <TextArea id="notesArea" rows={4} defaultValue="Change Notes" />
 
-                    <Link to="/">
-                        <Button type="primary" style= {divStyleRight} >Back to Home</Button>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+        <div>
+          <Button type="primary" onClick={this.updateNotes}>
+            Submit Notes
+          </Button>
+
+          <Link to="/">
+            <Button type="primary" style={divStyleRight}>
+              Back to Home
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotesPage;
